61e24e26ac7408ceea0fe9bb1240d0f47a16b41e
Artem <evdafe@gmail.com>
2023-09-18 13:10:35 +0300

    +ChangedText +NewHeartSprite

--- Assets/Scripts/WinAndDefeat/DefeatTimerText.cs
+++ Assets/Scripts/WinAndDefeat/DefeatTimerText.cs
@@ -5,11 +5,12 @@ using Zenject;
 public class DefeatTimerText : GameModeUIElement<TimeOutDefeatCondition>
 {
     [SerializeField] private TMP_Text _text;
+    [SerializeField] private string _prefix;
 
     private DefeatTimer _defeatTimer;
 
     [Inject]
     private void Construct(DefeatTimer defeatTimer) => _defeatTimer = defeatTimer;
 
-    private void FixedUpdate() => _text.text = Mathf.Ceil(_defeatTimer.TimeToDefeat).ToString();
+    private void FixedUpdate() => _text.text = _prefix + Mathf.Ceil(_defeatTimer.TimeToDefeat).ToString();
 }
